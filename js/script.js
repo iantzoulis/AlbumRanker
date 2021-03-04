98e5521e9efafe71c9b8f42e434ccf3b80178645
Ioannis A <39637230+iantzoulis@users.noreply.github.com>
2021-03-04 17:39:46 -0500

    Added touch support for mobile and delete column

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ $(function () {
     $("tbody").disableSelection();
 });
 
+$(document).on("click", ".del-icon", function () {
+    $(this).closest('tr.item').remove();
+    $('.rank').each(function (i) {
+        $(this).text(i + 1);
+    });
+});
+
 function addItem() {
     albumTitle = document.getElementById("album-input").value.trim();
     artist = document.getElementById("artist-input").value.trim();
@@ -18,27 +25,23 @@ function addItem() {
     var table = document.getElementById("ranker-table").getElementsByTagName('tbody')[0];
     var pos = table.rows.length;
     var row = table.insertRow(pos);
+    row.className = 'item';
 
     var rankCell = row.insertCell(0);
+    rankCell.className = 'rank';
+
     var albumCell = row.insertCell(1);
     var artistCell = row.insertCell(2);
+    var deleteCell = row.insertCell(3);
 
     rankCell.innerHTML = pos + 1;
     albumCell.innerHTML = albumTitle;
     artistCell.innerHTML = artist;
+    deleteCell.innerHTML = '<div class="text-center del-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" /></svg></div>';
 
     document.getElementById("album-input").value = '';
     document.getElementById("artist-input").value = '';
-
     document.querySelector("#album-input").focus();
-}
-
-function sortItems() {
-
-}
-
-function deleteItem() {
-
 }
 
 function confirmListDeletion() {
